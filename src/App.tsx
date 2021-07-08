20a7259441cca1281098966e987fc40f882c247a
cha777 <chathuranga_wm@yahoo.com>
2021-07-08 20:56:35 +0530

    🎨 added theme support

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
-const App = () => <h1>Welcome to Autozone</h1>;
+import { ThemeProvider } from '@material-ui/core';
+import { createAppTheme } from './theme';
+import GlobalStyles from './components/GlobalStyles';
+
+const App = () => {
+  const theme = createAppTheme({
+    responsiveFontSizes: true,
+    roundedCorners: true,
+    theme: 'DARK',
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import StyledEngineProvider from '@material-ui/core/StyledEngineProvider';
+
 import App from './App';
 
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <HelmetProvider>
+      <StyledEngineProvider injectFirst>
+        <App />
+      </StyledEngineProvider>
+    </HelmetProvider>
   </StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
